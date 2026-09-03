3eed7c97f01915c4c6bfe7078d3c2dd60cba6ddb
Antony Lewis <cmbant@users.noreply.github.com>
2026-09-03 10:19:56 +0100

    Import QI-C irreducibility certificates

--- FormalResearch.lean
+++ FormalResearch.lean
@@ -2,6 +2,7 @@ import FormalResearch.Certificates.FiniteField
 import FormalResearch.QIA.FourQubitMatrix
 import FormalResearch.QIA.PairingGram
 import FormalResearch.QIC.QubitTailDeterminants
+import FormalResearch.QIC.FiniteFieldIrreducibility
 import FormalResearch.QID.D5RankWitness
 import FormalResearch.QID.D5WeylEnumeration
 import FormalResearch.Gaudin.WeightedTreeMetric
