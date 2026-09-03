f77a0c0ea18c5e35fa850d262e283bd89479b734
Antony Lewis <cmbant@users.noreply.github.com>
2026-09-03 09:48:16 +0100

    Add D5 exact rank witness

---
+++ FormalResearch/QID/D5RankWitness.lean
@@ -0,0 +1,25 @@
+import Mathlib.Data.Matrix.Notation
+import Mathlib.LinearAlgebra.Matrix.Determinant.Basic
+
+namespace FormalResearch.QID
+
+def N5 : Matrix (Fin 5) (Fin 5) Int :=
+  !![ 1, -1,  1,  1,  0;
+      0, -2,  2,  0, -1;
+      0,  2, -2,  0, -1;
+     -1, -1, -1, -1,  0;
+      0,  0,  0,  0,  1]
+
+def N5minor : Matrix (Fin 4) (Fin 4) Int :=
+  !![ 1, -1,  1,  0;
+      0, -2,  2, -1;
+      0,  2, -2, -1;
+     -1, -1, -1,  0]
+
+theorem N5_det_zero : N5.det = 0 := by
+  native_decide
+
+theorem N5minor_det : N5minor.det = 8 := by
+  native_decide
+
+end FormalResearch.QID
