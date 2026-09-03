e44e10560343674cea5425442e79be3d86fc30db
Antony Lewis <cmbant@users.noreply.github.com>
2026-09-03 16:44:06 +0100

    Import Robinson-Foulds Hamming bridge

--- FormalResearch.lean
+++ FormalResearch.lean
@@ -50,6 +50,7 @@ import FormalResearch.QID.D5SupportingFacet
 import FormalResearch.QID.D5FullFacetCertificate
 import FormalResearch.Gaudin.WeightedTreeMetric
 import FormalResearch.Gaudin.WeightedTreeMetricSeparation
+import FormalResearch.Gaudin.RobinsonFouldsHammingBridge
 import FormalResearch.Gaudin.ExponentBookkeeping
 import FormalResearch.Gaudin.TreeSpaceRank
 import FormalResearch.Gaudin.CrossRankLinearAlgebra
