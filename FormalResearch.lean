4918cb26e56611649b886e02a8cf4b7ce96eaaf6
Antony Lewis <cmbant@users.noreply.github.com>
2026-09-03 13:41:31 +0100

    Import coherent apolar kernel theorem

--- FormalResearch.lean
+++ FormalResearch.lean
@@ -8,6 +8,7 @@ import FormalResearch.QIB1.ApolarPivot
 import FormalResearch.QIB1.ApolarTriangularRank
 import FormalResearch.QIB1.ApolarFullRank
 import FormalResearch.QIB1.ApolarKernelDimension
+import FormalResearch.QIB1.ApolarCoherentKernel
 import FormalResearch.QIB2.HammingGate
 import FormalResearch.QIB2.HammingSubsetExpansion
 import FormalResearch.QIB2.HammingSpectralPhase
