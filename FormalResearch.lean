c3a17e519cdb4fa51a3d85af8f4291060d046bde
Antony Lewis <cmbant@users.noreply.github.com>
2026-09-03 10:24:37 +0100

    Import QI-C Gaussian nonvanishing certificates

--- FormalResearch.lean
+++ FormalResearch.lean
@@ -3,6 +3,7 @@ import FormalResearch.QIA.FourQubitMatrix
 import FormalResearch.QIA.PairingGram
 import FormalResearch.QIC.QubitTailDeterminants
 import FormalResearch.QIC.FiniteFieldIrreducibility
+import FormalResearch.QIC.GaussianNonvanishing
 import FormalResearch.QID.D5RankWitness
 import FormalResearch.QID.D5WeylEnumeration
 import FormalResearch.QID.D5AffineRank
