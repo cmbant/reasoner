108f611b46c4a6cdc7b3fea5162ae01804e26c73
Antony Lewis <cmbant@users.noreply.github.com>
2026-09-03 18:02:00 +0100

    Import Blaschke composition energy transfer

--- FormalResearch.lean
+++ FormalResearch.lean
@@ -69,6 +69,7 @@ import FormalResearch.Gaudin.BernoulliInverseUniqueness
 import FormalResearch.Gaudin.BernoulliPrintedRecurrence
 import FormalResearch.Blaschke.CompositionBaseline
 import FormalResearch.Blaschke.CompositionStability
+import FormalResearch.Blaschke.CompositionEnergyTransfer
 import FormalResearch.Blaschke.CyclicChargeEnergy
 import FormalResearch.Blaschke.WronskianPair
 import FormalResearch.Blaschke.WalshBooleanCollapse
