49da168b41a03507a83bafce795c3e7d53db07b3
Antony Lewis <cmbant@users.noreply.github.com>
2026-09-03 09:48:07 +0100

    Add exact four-qubit chirality matrix core

---
+++ FormalResearch/QIA/FourQubitMatrix.lean
@@ -0,0 +1,19 @@
+import Mathlib.Data.Matrix.Notation
+import Mathlib.Data.Matrix.Basic
+
+namespace FormalResearch.QIA
+
+def chiralityK : Matrix (Fin 3) (Fin 3) Int :=
+  !![-1, -1, -2;
+      1,  1,  2;
+      1, -1,  0]
+
+theorem chiralityK_cubic : chiralityK ^ 3 = -(4 • chiralityK) := by
+  native_decide
+
+theorem chiralityK_ne_zero : chiralityK != 0 := by
+  intro h
+  have h00 := congrFun (congrFun h (0 : Fin 3)) (0 : Fin 3)
+  norm_num [chiralityK] at h00
+
+end FormalResearch.QIA
