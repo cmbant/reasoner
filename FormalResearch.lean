de44b80bda3684c1eee95cefba29feb2be9f5309
Antony Lewis <cmbant@users.noreply.github.com>
2026-09-03 13:08:12 +0100

    Import full apolar differential rank theorem

--- FormalResearch.lean
+++ FormalResearch.lean
@@ -6,6 +6,7 @@ import FormalResearch.QIA.PrintedChiralitySpectrum
 import FormalResearch.QIA.PairingToPrintedBasis
 import FormalResearch.QIB1.ApolarPivot
 import FormalResearch.QIB1.ApolarTriangularRank
+import FormalResearch.QIB1.ApolarFullRank
 import FormalResearch.QIB2.HammingGate
 import FormalResearch.QIB2.HammingSubsetExpansion
 import FormalResearch.QIB2.HammingSpectralPhase
