ece8057b2ca995c70cbe4d45a1312759a2482775
Antony Lewis <cmbant@users.noreply.github.com>
2026-09-03 10:34:08 +0100

    Import Gaussian integer lift certificates

--- FormalResearch.lean
+++ FormalResearch.lean
@@ -4,6 +4,7 @@ import FormalResearch.QIA.PairingGram
 import FormalResearch.QIC.QubitTailDeterminants
 import FormalResearch.QIC.FiniteFieldIrreducibility
 import FormalResearch.QIC.GaussianNonvanishing
+import FormalResearch.QIC.GaussianIntegerLift
 import FormalResearch.QID.D5RankWitness
 import FormalResearch.QID.D5WeylEnumeration
 import FormalResearch.QID.D5AffineRank
