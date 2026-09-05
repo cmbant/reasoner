c3615e223ba02a5b0c2315c78e6e7e4589f01b69
Antony Lewis <cmbant@users.noreply.github.com>
2026-09-05 22:00:57 +0100

    Split endpoint certificate sample 28

---
+++ FormalResearch/QIC/Endpoint14EliminationCertificate28.lean
@@ -0,0 +1,9 @@
+import FormalResearch.QIC.Endpoint14EliminationCertificateBase
+
+namespace FormalResearch.QIC
+
+lemma endpointElim_certificate28_single :
+    endpointElimCertificateAt (28 : EndpointSampleIndex) := by
+  native_decide
+
+end FormalResearch.QIC
