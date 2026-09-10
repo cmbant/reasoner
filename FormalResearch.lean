5562dd03dffb0b95734154cfb22c344668f76e82
Antony Lewis <cmbant@users.noreply.github.com>
2026-09-10 20:27:57 +0100

    Import multiplicity Hermitian trace pairing

--- FormalResearch.lean
+++ FormalResearch.lean
@@ -23,6 +23,7 @@ import FormalResearch.QIA.MultiplicityBlockClassicality
 import FormalResearch.QIA.CanonicalMemoryCarriers
 import FormalResearch.QIA.MatrixTracePairing
 import FormalResearch.QIA.HermitianInnerPairing
+import FormalResearch.QIA.MultiplicityHermitianTracePairing
 import FormalResearch.QIB.U1InvariantExponentGap
 import FormalResearch.QIB1.ApolarPivot
 import FormalResearch.QIB1.ApolarTriangularRank
