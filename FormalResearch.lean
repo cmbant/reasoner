597288e57670e4ddea8f4292ee20c229eb21b845
Antony Lewis <cmbant@users.noreply.github.com>
2026-09-10 07:34:17 +0100

    Import T07 code-dimension certificate

--- FormalResearch.lean
+++ FormalResearch.lean
@@ -14,6 +14,7 @@ import FormalResearch.QIA.ThreeCopyChiralityOnset
 import FormalResearch.QIA.ThreeCopyChiralityPersistence
 import FormalResearch.QIA.MultiplicityProfileDimensions
 import FormalResearch.QIA.MultiplicityProfileQuantumExcess
+import FormalResearch.QIA.SectorLabelOnlyCodeDimension
 import FormalResearch.QIB.U1InvariantExponentGap
 import FormalResearch.QIB1.ApolarPivot
 import FormalResearch.QIB1.ApolarTriangularRank
