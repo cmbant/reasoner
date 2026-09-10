822432ccfd54b7eaf5d6b88798c3cc357eb85c8d
Antony Lewis <cmbant@users.noreply.github.com>
2026-09-10 21:54:49 +0100

    Import exact-recovery Heisenberg memory bound

--- FormalResearch.lean
+++ FormalResearch.lean
@@ -24,6 +24,7 @@ import FormalResearch.QIA.CanonicalMemoryCarriers
 import FormalResearch.QIA.MatrixTracePairing
 import FormalResearch.QIA.HermitianInnerPairing
 import FormalResearch.QIA.MultiplicityHermitianTracePairing
+import FormalResearch.QIA.ExactRecoveryHeisenbergMemoryBound
 import FormalResearch.QIB.U1InvariantExponentGap
 import FormalResearch.QIB1.ApolarPivot
 import FormalResearch.QIB1.ApolarTriangularRank
