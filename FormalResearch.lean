959432cdced789903fdbac3fa2c4fdba2d40dee7
Antony Lewis <cmbant@users.noreply.github.com>
2026-09-10 18:55:41 +0100

    Import QI-A canonical memory carriers

--- FormalResearch.lean
+++ FormalResearch.lean
@@ -20,6 +20,7 @@ import FormalResearch.QIA.SectorLabelOnlyCodeDimension
 import FormalResearch.QIA.PureCopyMemoryCore
 import FormalResearch.QIA.CoherentWidthFactorization
 import FormalResearch.QIA.MultiplicityBlockClassicality
+import FormalResearch.QIA.CanonicalMemoryCarriers
 import FormalResearch.QIB.U1InvariantExponentGap
 import FormalResearch.QIB1.ApolarPivot
 import FormalResearch.QIB1.ApolarTriangularRank
