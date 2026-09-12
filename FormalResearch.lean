57d2bea6d17387b8113373cc621571be4800ecdc
Antony Lewis <cmbant@users.noreply.github.com>
2026-09-12 06:13:47 +0100

    QI-A: promote isotypic multiplicity reduction import

--- FormalResearch.lean
+++ FormalResearch.lean
@@ -29,6 +29,7 @@ import FormalResearch.QIA.MatrixTraceAdjointPOVM
 import FormalResearch.QIA.MatrixDensityUpperBound
 import FormalResearch.QIA.IsotypicAuxiliaryStates
 import FormalResearch.QIA.IsotypicDirectSumAuxiliaryStates
+import FormalResearch.QIA.IsotypicMultiplicityReduction
 import FormalResearch.QIA.SymmetricMultilinearDiagonal
 import FormalResearch.QIA.CoherentPowerSymmetricSpan
 import FormalResearch.QIA.CoherentPhaseOrthogonality
