c311d764cb276b243fd9386514ee002688790f4b
Antony Lewis <cmbant@users.noreply.github.com>
2026-09-12 08:49:02 +0100

    QI-A: promote isotypic block positivity module

--- FormalResearch.lean
+++ FormalResearch.lean
@@ -30,6 +30,7 @@ import FormalResearch.QIA.MatrixDensityUpperBound
 import FormalResearch.QIA.IsotypicAuxiliaryStates
 import FormalResearch.QIA.IsotypicDirectSumAuxiliaryStates
 import FormalResearch.QIA.IsotypicMultiplicityReduction
+import FormalResearch.QIA.IsotypicMultiplicityReductionPositivity
 import FormalResearch.QIA.SymmetricMultilinearDiagonal
 import FormalResearch.QIA.CoherentPowerSymmetricSpan
 import FormalResearch.QIA.CoherentPhaseOrthogonality
