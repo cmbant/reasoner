5251e614a250d5a141b67042ac02dfb9ca045f3d
Antony Lewis <cmbant@users.noreply.github.com>
2026-09-13 07:15:16 +0100

    QI-A: promote finite isotypic carrier coordinates

--- FormalResearch.lean
+++ FormalResearch.lean
@@ -39,6 +39,7 @@ import FormalResearch.QIA.CStarMatrixBlockFlattenPositivity
 import FormalResearch.QIA.IsotypicMultiplicityReductionCompletelyPositive
 import FormalResearch.QIA.IsotypicBaseFieldCoordinates
 import FormalResearch.QIA.GlobalIsotypicBaseFieldDecomposition
+import FormalResearch.QIA.FiniteIsotypicCarrierCoordinates
 import FormalResearch.QIA.SymmetricMultilinearDiagonal
 import FormalResearch.QIA.CoherentPowerSymmetricSpan
 import FormalResearch.QIA.CoherentPhaseOrthogonality
